632847093b873b44fa9a202af42ab564e2aea70c
YuanPre <balalaxmxtlm@icloud.com>
2017-04-14 15:34:57 +0800

    Add main program

---
+++ bala.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Set Variables
+homesh="$HOME/sh"
+mkd="/bin/mkdir -p"
+readp="read -p"
+ece="/bin/echo -e"
+ec_bb="\033[46;30m"
+ec_wr="\033[47;31m"
+ec_0="\033[0m"
+ec_pr="$0 ERROR: Permission Denied"
+ec_yn="Please enetr Y or N."
+
+# Welcome and Copyright
