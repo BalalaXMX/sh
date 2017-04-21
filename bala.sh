e999488f044dbf44bf2434344c0325837364f012
YuanPre <balalaxmxtlm@icloud.com>
2017-04-21 08:33:57 +0800

    Updates

--- bala.sh
+++ bala.sh
@@ -12,3 +12,19 @@ ec_pr="$0 ERROR: Permission Denied"
 ec_yn="Please enetr Y or N."
 
 # Welcome and Copyright
+$ece ""$ec_bb"XMX Number Circulator"$ec_0"\n"
+echo "Copyright (C) 2017 YuanYuan in BLMF club"
+echo "Lincesed under by GNU GPLv3+: GNU GPL 3 and later <http://www.gnu.org/lincese/>"
+$ece "This is free software; you are free to change and redistribute it.\n"
+
+# Get Parameter
+$readp "Start= " strI
+$readp "End= " endI
+$readp "Step= " stpI
+echo ""
+$readp "Operation= " optI
+if [ $optI -eq 1 ]; then
+    $readp "Enter text you want to dispaly: " texI
+    $readp "Warp? (Y or N): " wrpI
+    $readp "Text of endline: " enlI
+fi
